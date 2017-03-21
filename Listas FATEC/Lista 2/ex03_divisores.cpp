f6a0d14cc1fb3908e89b8a8c5f2b498429994807
augustoliks <carlos.santos110@fatec.sp.gov.br>
2017-03-21 08:54:04 -0300

    C

---
+++ Listas FATEC/Lista 2/ex03_divisores.cpp
@@ -0,0 +1,40 @@
+/*
+	1. Retorna 1 se for PRIMO
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int primo(int m){
+	
+	int ak=0, k=1, op=1;
+			
+	for (k ; k<=m ; k++){
+		if (!(m%k)) {
+			printf("Divisivel por: %d\n", k);
+			ak+=1;
+		}	
+	}
+		
+
+	return ak;	
+	
+}
+
+int main(){
+	
+	int m, dec;
+	
+	do{
+		scanf("%d", & m);
+			
+		if(m<1){ 
+			printf("Valores negativos nao permitidos, indique outro valor: "); 
+		}
+
+	} while(m<1);
+	
+	printf( "%d", primo(m) );
+	
+}
