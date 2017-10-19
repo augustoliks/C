3770519687a971d179d8a49d400f6876b500184d
augustoliks <carlos.santos110@fatec.sp.gov.br>
2017-10-19 14:28:11 -0200

    Correcao no Enunciado

--- Ponteiro/ordena_matriz.c
+++ Ponteiro/ordena_matriz.c
@@ -1,9 +1,9 @@
 /*
-Faça um programa que, dado um inteiro n e uma matriz quadrada de ordem n, cujos
-elementos são todos inteiros positivos, imprime uma tabela onde os elementos são
-listados em ordem decrescente, acompanhados da indicação de linha e coluna a que
-pertencem. Havendo repetições de elementos na matriz, a ordem é irrelevante. Utilize
-obrigatoriamente a função da parte (a), mesmo que você não a tenha feito
+Faca um programa que, dado um inteiro n e uma matriz quadrada de ordem n, cujos
+elementos sao todos inteiros positivos, imprime uma tabela onde os elementos sao
+listados em ordem decrescente, acompanhados da indicao de linha e coluna a que
+pertencem. Havendo repeticoes de elementos na matriz, a ordem eh irrelevante. Utilize
+obrigatoriamente a funcoes da parte (a), mesmo que voce nao a tenha feito
 */
 
 #include <stdio.h>
@@ -42,10 +42,10 @@ int main(){
 
 	printf("Elem\tLinha\tColuna\n");
 
-    for(x=x*x ; x>0 ; x--){
-        maior(matriz, n, &K, &LIN, &COL);
+	for(x=x*x ; x>0 ; x--){
+		maior(matriz, n, &K, &LIN, &COL);
 		printf("%d\t%d\t%d\n", K, LIN+1, COL+1);
 		matriz[LIN][COL]= -1;
-    }
+	}
 
 }
